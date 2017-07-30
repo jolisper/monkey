5959f24a790916fe577cbea36fedd3219d2eb2eb
Jorge Luis Pérez <jolisper@gmail.com>
2017-07-30 16:22:25 -0300

    Added the foundation of the object system: integer, boolean and null objects

---
+++ object/object.go
@@ -0,0 +1,53 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+// Integer object
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+func (i *Integer) Inspect() string {
+	fmt.Sprintf("%d", i.Value)
+}
+
+//Boolean object
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b *Boolean) Inspect() string {
+	fmt.Sprintf("%b", b.Value)
+}
+
+// Null object
+type Null struct{}
+
+func (n *Null) Type() ObjectType {
+	return NULL_OBJ
+}
+
+func (n *Null) Inspect() string {
+	return "null"
+}
