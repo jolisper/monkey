16d956c35bd60d6e4b4e7fec2885fdcf37a3e3b5
Jorge Luis Pérez <jolisper@gmail.com>
2017-08-02 01:41:20 -0300

    Completing the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/jolisper/monkey/evaluator"
 	"github.com/jolisper/monkey/lexer"
 	"github.com/jolisper/monkey/parser"
 )
@@ -44,8 +45,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
