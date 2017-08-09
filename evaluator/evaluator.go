0e1b24510e76425c1147aafa2de223a936dd6438
Jorge Luis Pérez <jolisper@gmail.com>
2017-08-08 22:31:06 -0300

    Added support for equality operators on booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooleanToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooleanToBooleanObject(left != right)
 	default:
 		return NULL
 	}
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -67,6 +67,18 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"1 != 1", false},
 		{"1 == 2", false},
 		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"false == true", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"true != true", false},
+		{"false != false", false},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == false", true},
+		{"(1 > 2) == true", false},
 	}
 
 	for _, tt := range tests {
